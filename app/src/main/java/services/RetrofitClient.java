611233dc9f397b2555648c8fc7cd21bd12985f2d
vbonev <vbonev@virtual-affairs.com>
2017-07-13 10:11:44 +0300

    Added Interceptor to log the response

--- app/src/main/java/services/RetrofitClient.java
+++ app/src/main/java/services/RetrofitClient.java
@@ -1,7 +1,10 @@
 package services;
 
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
 
 /**
  * Created by dkirilova on 7/12/2017.
@@ -12,11 +15,17 @@ public class RetrofitClient {
     public static final String BASE_URL = "https://demo7551154.mockable.io";
     public static Retrofit retrofit = null;
 
-    public static Retrofit getRetrofitClient(){
+    public static Retrofit getRetrofitClient() {
+        okhttp3.OkHttpClient.Builder builder = new okhttp3.OkHttpClient.Builder();
+        builder.addInterceptor(new HttpLoggingInterceptor().setLevel(BODY));
+        okhttp3.OkHttpClient client = builder.build();
+        if (retrofit == null) {
 
-        if(retrofit == null){
-
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+            retrofit = new Retrofit.Builder()
+                    .client(client)
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
         return retrofit;
     }
